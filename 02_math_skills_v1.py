52ea36c019aaeb70c2edce9eeb8b6d616f94cf9c
milnux <milner.brook@awatapu.school.nz>
2021-09-27 11:47:08 +1300

    initial commit

--- 02_math_skills_v1.py
+++ 02_math_skills_v1.py
@@ -5,6 +5,7 @@
 end_game = ""
 while end_game != "xxx":
     math_skills = int(input("How confident are you in maths on a scale of 1 to 10?"))
+# If math skills 10
 
     if math_skills <= 3:
         print("bad at maths")
