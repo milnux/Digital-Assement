7bd54b3f8ef822ebaffb0f59f1177ac4d0f73b21
milnux <milner.brook@awatapu.school.nz>
2021-10-01 09:28:53 +1300

    math skills running correctly, needa add questions inside

--- 02_math_skills_v1.py
+++ 02_math_skills_v1.py
@@ -5,14 +5,13 @@ end_game = ""
 while end_game != "xxx":
     math_skills = int(input("How confident are you in maths on a scale of 1 to 10?"))
     # If math skills is 1, 2 or 3, display easy questions
-    if math_skills <= 3:
-        print("not good at maths")
+    if math_skills >= 7 <= 10:
+        print("great at maths")
     # if math skills is 4 5 or 6, display medium questions
     elif math_skills >= 4 <= 6:
         print("decent at maths")
     # if math skills is 8 9 or 10, display hard questions
-    elif math_skills >= 7 < 11:
-        print("great at maths")
+    elif math_skills > 0 <= 3:
+        print("not good at maths")
     else:
         print("[ERROR] Please put a whole number")
-        end_game = "xxx"
