3e845f22f85c48d15b341d1ff5cd188c2f93d6bd
milnux <milner.brook@awatapu.school.nz>
2021-11-04 09:37:55 +1300

    version 2

---
+++ 04_question_ask_v2.py
@@ -0,0 +1,18 @@
+def quest_ask(question, answer):
+    error = "Enter a number"
+    valid = False
+    while not valid:
+        try:
+            response = int(input(question))
+            if response == answer:
+                print("Correct")
+                return response
+            else:
+                print("Incorrect")
+                return response
+        except ValueError:
+            print(error)
+
+# Main routine...
+
+quest_ask("what is 2 x 4", 8)
